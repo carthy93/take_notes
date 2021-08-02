cc425703c45e9bdf20844a4598aee30d5417e989
Karthik Balachandar <caarthik180@gmail.com>
2021-08-02 22:31:38 +0530

    codes addes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
+import NotesList from "./components/NotesList";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
+const App = () => {
+  const [notes, setnotes] = useState([
+    {
+      id: nanoid(),
+      text: "First Note",
+      date: "02/08/2021",
+    },
+    {
+      id: nanoid(),
+      text: "Second Note",
+      date: "03/08/2021",
+    },
+    {
+      id: nanoid(),
+      text: "Third Note",
+      date: "04/08/2021",
+    },
+  ]);
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <h2> simple react project</h2>
-      <h3>Changes coming</h3>
+      <NotesList notes={notes} />
     </div>
   );
-}
+};
 
 export default App;
